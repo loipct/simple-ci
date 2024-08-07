3cd8adb30dde3cc6eebc903bf5088f2eaae709af
loiprocute <loi.nguyen31.2001@hcmut.edu.vn>
2024-08-07 09:57:07 +0700

    rename files

--- sum.py
+++
@@ -1,9 +0,0 @@
-# main.py
-def add_two_numbers(a, b):
-    return a + b
-
-if __name__ == "__main__":
-    a = float(input("Nhập số a: "))
-    b = float(input("Nhập số b: "))
-    result = add_two_numbers(a, b)
-    print(f"Tổng của {a} và {b} là {result}")
