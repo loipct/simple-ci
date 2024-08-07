e6bd925015f746cb04e258679d13c80a6b1cd4bb
loiprocute <loi.nguyen31.2001@hcmut.edu.vn>
2024-08-07 10:10:10 +0700

    add test file

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,8 @@
 # tests/test_main.py
+import sys
+import os
+# Thêm thư mục gốc vào sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from simple_sum import add_two_numbers
 
 def test_add_two_numbers():
